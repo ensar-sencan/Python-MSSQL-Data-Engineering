b0381fd4cc98f06e707bc5836540ae567bc7a638
Ensar Şencan <ensar61826@gmail.com>
2026-02-19 00:04:58 +0300

    Update canli_veri.py

--- canli_veri.py
+++ canli_veri.py
@@ -2,25 +2,24 @@ import requests
 import pandas as pd
 import sqlalchemy
 import urllib
-import time # Zamanı kontrol etmek için
+import time 
 from datetime import datetime
 
 # --- AYARLAR ---
-Server = r'.\SQLEXPRESS05' # Senin sunucu adın
+Server = r'.\SQLEXPRESS05' 
 Database = 'DogusStaj'
 Driver = 'ODBC Driver 17 for SQL Server'
 Tablo_Adi = 'Kripto_Fiyatlari'
 
-# Bağlantı Motorunu Kur (Döngünün dışında 1 kere kurmak yeterli)
+# Bağlantı
 conn_str = f'DRIVER={{{Driver}}};SERVER={Server};DATABASE={Database};Trusted_Connection=yes;'
 quoted_conn_str = urllib.parse.quote_plus(conn_str)
 engine = sqlalchemy.create_engine(f'mssql+pyodbc:///?odbc_connect={quoted_conn_str}')
 
-print("🚀 KRİPTO TAKİP BOTU BAŞLATILIYOR...")
+print(" KRİPTO TAKİP BOTU BAŞLATILIYOR...")
 print("Her 5 saniyede bir veri çekecek. Çıkmak için CTRL+C yapabilirsin.\n")
 
-# --- SONSUZ DÖNGÜ (veya Sayılı) ---
-for i in range(1, 61): # 60 kere çalışsın (Yaklaşık 5 dakika sürer)
+for i in range(1, 61): # 60 kere çalışsını istedim.
     try:
         # 1. Veriyi Çek
         url = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
@@ -40,13 +39,13 @@ for i in range(1, 61): # 60 kere çalışsın (Yaklaşık 5 dakika sürer)
         # 3. SQL'e Bas
         df.to_sql(Tablo_Adi, con=engine, if_exists='append', index=False)
         
-        print(f"[{i}/60] ✅ {tarih} - BTC: {fiyat} $ -> Kaydedildi.")
+        print(f"[{i}/60]  {tarih} - BTC: {fiyat} $ -> Kaydedildi.")
         
         # 4. Bekle (5 Saniye)
         time.sleep(5)
         
     except Exception as e:
-        print(f"❌ Hata oluştu: {e}")
-        time.sleep(5) # Hata alsa bile 5 saniye bekle, durma
+        print(f" Hata oluştu: {e}")
+        time.sleep(5) 
 
-print("\n🏁 GÖREV TAMAMLANDI! Veritabanı veriye doydu.")
+print("\n GÖREV TAMAMLANDI! Veritabanı veriye doydu.")
